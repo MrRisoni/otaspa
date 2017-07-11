f428e2f3ad0ea74ff2b6ccfd93703e6b963dc378
phoenix <vanguelisk@gmail.com>
2017-07-11 22:45:03 +0300

    insurances

--- src/containers/Passenger/Insurance/InsuranceBox.js
+++ src/containers/Passenger/Insurance/InsuranceBox.js
@@ -2,7 +2,34 @@ import React, {Component} from 'react';
 
 
 class InsuranceBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            localid : 0
+        };
+        this.localChange = this.localChange.bind(this);
+    }
+
+    localChange(event) {
+        console.log(event.target.value);
+        this.props.updateParentHandler(event.target.value);
+    }
+
+
+    componentDidMount()
+    {
+        var self = this;
+        self.setState({localid : this.props.data.id});
+
+    }
     render() {
+
+        console.log(this.props);
+        console.log(this.props.data);
+
+
+
+
         return (<div className="col-md-3">
 
             <div className="panel panel-default">
@@ -23,7 +50,11 @@ class InsuranceBox extends Component {
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-4">
-                            <input type="radio" value="option1" checked={true}/>
+                            <input type="radio"
+                                   value={this.props.data.id}
+                                   checked={this.props.selected === this.state.localid}
+                                   onChange={this.localChange}
+                                   />
                         </div>
                     </div>
 
--- src/containers/Passenger/Insurance/InsurancePanel.js
+++ src/containers/Passenger/Insurance/InsurancePanel.js
@@ -6,14 +6,19 @@ class InsurancePanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedOption: 0
+            selectedOption: 1
         };
 
         this.handleOptionChange = this.handleOptionChange.bind(this);
     }
 
-    handleOptionChange() {
+    handleOptionChange(ev) {
 
+        var self = this;
+        console.log(ev.target.value);
+        self.setState({selectedOption: ev.target.value});
+
+        console.log(this.state);
     }
 
     render() {
@@ -26,7 +31,39 @@ class InsurancePanel extends Component {
 
                             <div className="row">
                                 {this.props.insuranceInfo.map((item, index) => {
-                                    return (<InsuranceBox data={item}/>)
+                                    {/* Insurance Box Component */}
+                                    return ( <div className="col-md-3">
+
+                                        <div className="panel panel-default">
+                                            <div className="panel-body">
+                                                <div className="row">
+                                                    <div className="col-md-12">
+                                                        <p> {item.title}</p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="row">
+                                                    <div className="col-md-4"></div>
+                                                    <div className="col-md-4">
+                                                        {item.price}
+                                                    </div>
+                                                </div>
+
+                                                <div className="row">
+                                                    <div className="col-md-4"></div>
+                                                    <div className="col-md-4">
+                                                        <input type="radio"
+                                                               value={item.id}
+                                                               checked={this.state.selectedOption == item.id}
+                                                               onChange={this.handleOptionChange}
+                                                        />
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+                                        </div>
+                                    </div>)
                                 })}
                             </div>
 
