e28a1aac34f486e081e9b751c147d8b20f90bb87
phoenix <vanguelisk@gmail.com>
2017-07-16 21:21:38 +0300

    fixes

--- src/ResultPage/ResultSet/ResultCompo.js
+++ src/ResultPage/ResultSet/ResultCompo.js
@@ -3,7 +3,18 @@ import React, {Component}  from 'react';
 import ResultLeg from './ResultLeg';
 
 class ResultCompo extends Component {
+    constructor(props)
+    {
+        super(props);
 
+        this.proceed = this.proceed.bind(this);
+    }
+
+    proceed()
+    {
+        this.props.router.push('/some/path');
+
+    }
     render() {
         return (
 
@@ -45,11 +56,11 @@ class ResultCompo extends Component {
                 <div className="panel-footer">
                     <div className="row">
                         <div className="col-md-5">
-                            Price {this.props.data.totalPrice}
+                            <b> Price {this.props.data.totalPrice} </b>
                         </div>
                         <div className="col-md-4"></div>
                         <div className="col-md-3">
-                            <button className="btn btn-sm btn-primary">Continue</button>
+                            <button className="btn  btn-primary" onClick={this.proceed}>Continue</button>
                         </div>
                     </div>
                 </div>
