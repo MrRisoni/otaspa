987454f88eb38803511b4873aa41c9cf7e071ed4
phoenix <vanguelisk@gmail.com>
2017-08-20 08:32:08 +0300

    bootstrap 4

--- src/PriceBox.js
+++ src/PriceBox.js
@@ -5,18 +5,44 @@ class PriceBox extends Component {
 
     render() {
         return (
+            <div className="pricebox">
 
-            <div className="row">
-                Show: {this.props.paxTypes[0].type}
+                <div className="card bg-info">
+                    <div className="card-header"><b>Price Analysis </b></div>
+                    <div className="card-body text-white">
 
-            </div>)
+                        <div className="row">
+
+                            <div className="col-md-12">
+                                <h4 className="analysisCategory">Ticket Price</h4>
+                                <hr/>
+                            </div>
+                        </div>
+
+
+                        Show: {this.props.paxTypes[0].type}
+                        {this.props.paxTypes.map((px) => {
+                            return (<div className="row">
+                                <div className="col-md-12">
+                                    {px.count} x {px.name} {px.convertedPrice} {this.props.currency}
+                                </div>
+                            </div>)
+                        })}
+
+                    </div>
+                </div>
+            </div>
+
+
+        )
     }
 }
 
 
 function mapStateToProps(state) {
     return {
-        paxTypes: state.paxTypes
+        paxTypes: state.paxTypes,
+        currency: state.selectedCurrency,
     };
 }
 
