b38e2a126e610c27e374b35ba1b70f93a67c4804
phoenix <vanguelisk@gmail.com>
2017-08-27 09:14:48 +0300

    segments

--- src/Itinerary/Leg.js
+++ src/Itinerary/Leg.js
@@ -4,21 +4,21 @@ import {observer, inject} from 'mobx-react';
 import moment from 'moment';
 
 import Airport from './Airport';
+import Segment from './Segment';
+
 
 @inject('otastore')
 @observer
 class Leg extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super();
 
         this.getAirportData = this.getAirportData.bind(this);
     }
 
-    getAirportData(segments)
-    {
+    getAirportData(segments) {
 
-        const last_seg = segments.length -1;
+        const last_seg = segments.length - 1;
         return {
             depIATA: segments[0].fromAirport.substr(0, 3),
             arrIATA: segments[last_seg].toAirport.substr(0, 3),
@@ -26,11 +26,11 @@ class Leg extends Component {
             depAirport: segments[0].fromAirport.substr(5),
             arrAirport: segments[last_seg].toAirport.substr(5),
 
-            depTime: segments[0].depDateTime.substr(11,5),
+            depTime: segments[0].depDateTime.substr(11, 5),
             depDay: moment(segments[0].depDateTime).format('dddd'),
             depDate: moment(segments[0].depDateTime).format('DD MMMM YYYY'),
 
-            arrTime: segments[last_seg].arrDateTime.substr(11,5),
+            arrTime: segments[last_seg].arrDateTime.substr(11, 5),
             arrDay: moment(segments[last_seg].arrDateTime).format('dddd'),
             arrDate: moment(segments[last_seg].arrDateTime).format('DD MMMM YYYY')
         }
@@ -38,15 +38,18 @@ class Leg extends Component {
 
     render() {
 
-        const stops = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.stops :   this.props.otastore.itinerary.info.return.stops;
+        const stops = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.stops : this.props.otastore.itinerary.info.return.stops;
 
-        const hours = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.waitTime.hours :   this.props.otastore.itinerary.info.return.waitTime.hours;
-        const minutes = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.waitTime.minutes :   this.props.otastore.itinerary.info.return.waitTime.minutes;
+        const hours = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.waitTime.hours : this.props.otastore.itinerary.info.return.waitTime.hours;
+        const minutes = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.waitTime.minutes : this.props.otastore.itinerary.info.return.waitTime.minutes;
 
-        const whours = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.durationTime.hours :   this.props.otastore.itinerary.info.return.durationTime.hours;
-        const wminutes = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.durationTime.minutes :   this.props.otastore.itinerary.info.return.durationTime.minutes;
+        const whours = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.durationTime.hours : this.props.otastore.itinerary.info.return.durationTime.hours;
+        const wminutes = (this.props.leg === 0) ? this.props.otastore.itinerary.info.departure.durationTime.minutes : this.props.otastore.itinerary.info.return.durationTime.minutes;
 
-        let obj = (this.props.leg ===0) ? this.getAirportData(this.props.otastore.itinerary.depSegments) :  this.getAirportData(this.props.otastore.itinerary.retSegments) ;
+        let obj = (this.props.leg === 0) ? this.getAirportData(this.props.otastore.itinerary.depSegments) : this.getAirportData(this.props.otastore.itinerary.retSegments);
+
+
+        let segments = (this.props.leg === 0) ? this.props.otastore.itinerary.depSegments : this.props.otastore.itinerary.retSegments;
 
 
         return (
@@ -90,14 +93,20 @@ class Leg extends Component {
                             </div>
 
                             <div className="col-md-4">
-                                Duration: {hours}h  {minutes}m
+                                Duration: {hours}h {minutes}m
                             </div>
 
                             <div className="col-md-4">
-                                Wait time: {whours}h  {wminutes}m
+                                Wait time: {whours}h {wminutes}m
                             </div>
                         </div>
 
+
+                        <div className="segments">
+                            {segments.map((sg, idx) => {
+                                return (<Segment key={idx} segData={sg}/>)
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Itinerary/Segment.js
+++ src/Itinerary/Segment.js
@@ -1,14 +1,67 @@
 import React, {Component} from 'react';
 
+import moment from 'moment';
+
+import Airport from './Airport';
+
 
 class Segment extends Component {
 
+    constructor(props)
+    {
+        super();
+
+        this.getSegmentData = this.getSegmentData.bind(this);
+    }
+
+    getSegmentData(segment)
+    {
+
+        return {
+            depIATA: segment.fromAirport.substr(0, 3),
+            arrIATA: segment.toAirport.substr(0, 3),
+
+            depAirport: segment.fromAirport.substr(5),
+            arrAirport: segment.toAirport.substr(5),
+
+            depTime: segment.depDateTime.substr(11,5),
+            depDay: moment(segment.depDateTime).format('dddd'),
+            depDate: moment(segment.depDateTime).format('DD MMMM YYYY'),
+
+            arrTime: segment.arrDateTime.substr(11,5),
+            arrDay: moment(segment.arrDateTime).format('dddd'),
+            arrDate: moment(segment.arrDateTime).format('DD MMMM YYYY')
+        }
+    }
+
     render() {
+
+
+        let obj = this.getSegmentData(this.props.segData);
+
         return (
 
 
+                <div className="row">
+                    <div className="col-md-1"></div>
 
-            <div> Component Segment </div>
+
+
+                        <Airport iata={obj.depIATA}
+                                 name={obj.depAirport}
+                                 flyTime={obj.depTime}
+                                 day={obj.depDay}
+                                 date={obj.depDate}/>
+
+                        <Airport iata={obj.arrIATA}
+                                 name={obj.arrAirport}
+                                 flyTime={obj.arrTime}
+                                 day={obj.arrDay}
+                                 date={obj.arrDate}/>
+
+                </div>
+
+
 
         );
     }
