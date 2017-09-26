e94894927acf2dd5938b1a997e4997d5b5d4a64a
phoenix <vanguelisk@gmail.com>
2017-09-26 09:21:16 +0300

    forward button

--- src/Booking/Passenger/UpgradeFare/FareCarousel.js
+++ src/Booking/Passenger/UpgradeFare/FareCarousel.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 
+import FontAwesome from 'react-fontawesome';
+
 
 class FareCarousel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0
+
+        };
+
+        this.nextItem = this.nextItem.bind(this);
+
+    }
+
+    nextItem()
+    {
+
+        var self = this;
+        let idx = self.state.index;
+        idx++;
+
+        if (idx === this.props.data.options.length) {
+            idx =0;
+        }
+
+        self.setState({index : idx});
+   }
 
     render() {
         return (
 
-            <div className="row">
+            <div className="row fareCarousel">
                 <div className="col-md-12">
                     <div className="card">
 
                         <div className="card-header">
-                            Select fare type
+                            {this.props.data.carrier}
                         </div>
 
 
@@ -19,7 +45,20 @@ class FareCarousel extends Component {
 
                             <div className="row">
 
-                                <div className="col-md-2">  prev  </div>
+                                <div className="col-md-2">
+
+
+                                    <button className="btn btn-sm">
+                                        <FontAwesome
+                                            className='fa fa-backward'
+                                            name='backward'
+                                            size='2x'
+                                            style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}
+                                        />
+                                    </button>
+
+
+                                </div>
 
                                 <div className="col-md-8">
 
@@ -28,7 +67,10 @@ class FareCarousel extends Component {
                                     <div className="card">
 
                                         <div className="card-header">
-                                            Flex Classic
+                                            {this.props.data.options[this.state.index].name}
+
+                                            {this.props.data.options[this.state.index].convertedPrice}
+
                                         </div>
 
                                         <div className="card-body">
@@ -42,7 +84,18 @@ class FareCarousel extends Component {
 
                                 </div>
 
-                                <div className="col-md-2">  next </div>
+                                <div className="col-md-2">
+
+                                    <button className="btn btn-sm" onClick={this.nextItem}>
+                                        <FontAwesome
+                                            className='fa fa-forward'
+                                            name='forward'
+                                            size='2x'
+                                            style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}
+                                        />
+                                    </button>
+
+                                </div>
 
 
                             </div>
