cc05a74da4f7f94d951a4a17afb41b7b3fe788bf
phoenix <vanguelisk@gmail.com>
2017-11-13 00:14:47 +0200

    enzyme tests

--- src/__tests__/PassengerList-test.js
+++ src/__tests__/PassengerList-test.js
@@ -5,18 +5,41 @@ configure({ adapter: new Adapter() });
 
 import {shallow, mount, render} from 'enzyme';
 
+import jsdom from 'jsdom'
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
+
 import PassengerList from '../Booking/PassengerList';
 import Passenger from '../Booking/Passenger';
 
 
 describe('A suite', function () {
-    it('should render without throwing an error', function () {
-        expect(shallow(<PassengerList/>).contains("Fill the passenger data")).toBe(true);
+
+    let testStore = {};
+
+    beforeEach( () => {
+        testStore = {
+            passengers: [
+                {value: "todo1", id: 111, complete: false},
+                {value: "todo2", id: 222, complete: false},
+                {value: "todo3", id: 333, complete: false},
+            ]
+        }
     });
 
 
+/*
+    it('should render without throwing an error', function () {
+        console.log('the store');
+        console.log(testStore);
+        expect(shallow(<PassengerList otastore={testStore} />).contains("Fill the passenger data")).toBe(true);
+    });
+*/
+
+
     it('should have  2 passengers', function () {
-        const wrapper = mount(<PassengerList/>);
+        const wrapper = mount(<PassengerList otastore={testStore}/>);
         expect(wrapper.find('.passengerCompo').length).toBe(2);
         expect(wrapper.find(Passenger).length).toBe(2);
     });
@@ -24,7 +47,7 @@ describe('A suite', function () {
 
 
     it('should have  3 passengers', function () {
-        const wrapper = mount(<PassengerList/>);
+        const wrapper = mount(<PassengerList otastore={testStore}/>);
         expect(wrapper.find('.passengerCompo').length).toBe(2);
         expect(wrapper.find(Passenger).length).toBe(2);
 
