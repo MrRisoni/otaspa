ad7b1d426be42c8ae279627b40af8c2033a9df2f
phoenix <vanguelisk@gmail.com>
2017-12-29 19:31:41 +0200

    map

--- src/Booking/Search/MyMapComponent.js
+++ src/Booking/Search/MyMapComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker , MarkerWithLabel} from "react-google-maps"
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
@@ -7,6 +7,15 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
         defaultCenter={{ lat: 38.3712, lng: 24.1268 }}
     >
         {props.isMarkerShown && <Marker position={{ lat: 37.9756, lng: 23.73379 }} />}
+
+        <MarkerWithLabel
+            position={{ lat: -34.397, lng: 150.644 }}
+            labelAnchor={new google.maps.Point(0, 0)}
+            labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+        >
+            <div>Hello There!</div>
+        </MarkerWithLabel>
+
     </GoogleMap>
 ));
 
